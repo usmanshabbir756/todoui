3cff6bd5697012276bd99f4218db8771e43f8259
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-03-12 14:45:02 +0500

    Route the component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderCompnent from './components/HeaderCompnent'
 import ListTodoComponent from './components/ListTodoComponent'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderCompnent />
-      <ListTodoComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderCompnent />
+        <Routes>
+          <Route path='/' element={<ListTodoComponent />}></Route>
+          <Route path='/todos' element={<ListTodoComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
