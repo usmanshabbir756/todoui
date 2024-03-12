c51d58ea13bdc5e192b5668e248afad71b4527e5
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-03-12 14:25:34 +0500

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderCompnent from './components/HeaderCompnent'
 import ListTodoComponent from './components/ListTodoComponent'
 
 function App() {
 
   return (
     <>
+      <HeaderCompnent />
       <ListTodoComponent />
+      <FooterComponent />
     </>
   )
 }
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div className=''>
+        <footer className='footer navbar-dark bg-dark'>
+            <p className='text-center'>CopyRights reserver @2024</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
